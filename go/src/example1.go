42b722048b1766df787cda562abb4a66b451c011
Bergquist, Connie <ameliorable.ckb@gmail.com>
2017-12-06 11:31:12 -0600

    Updated example1

--- go/src/example1.go
+++ go/src/example1.go
@@ -4,13 +4,18 @@ import "fmt"
 
 func main() {
 	// Declare the following variables with zero values:
+	// not zero but no value
 	// name: command, type string
 	// name: valid, type bool
-	command := "0"
-	valid := 0
+	//command := "0"
+	//valid := 0
+	var command string
+	var valid bool
+
 
 	// print the value of those variables
-    fmt.Println(command, valid)
+    fmt.Printf("command = %s\n", command)
+    fmt.Printf("valid = %v\n", valid)
 
 	// declare and initialize the following variables:
 	// name: foo, type: int, value: 5
@@ -19,5 +24,7 @@ func main() {
     bar := true
 
 	// print the value of those variables
-    fmt.Println(foo, bar)
+    fmt.Printf("foo = %d\n", foo)
+    fmt.Printf("bar = %v\n", bar)
+
 }
