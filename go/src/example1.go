75fc53001844c64c20562c7e0fd315af0921918c
Bergquist, Connie <ameliorable.ckb@gmail.com>
2017-12-06 11:27:51 -0600

    example 1

---
+++ go/src/example1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare the following variables with zero values:
+	// name: command, type string
+	// name: valid, type bool
+	command := "0"
+	valid := 0
+
+	// print the value of those variables
+    fmt.Println(command, valid)
+
+	// declare and initialize the following variables:
+	// name: foo, type: int, value: 5
+	// name bar, type: bool, value: true
+    foo := 5
+    bar := true
+
+	// print the value of those variables
+    fmt.Println(foo, bar)
+}
