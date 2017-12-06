9a00d2827a08c434189d4365f23582f7afe38a76
Bergquist, Connie <ameliorable.ckb@gmail.com>
2017-12-06 11:49:36 -0600

    Example 2

---
+++ go/src/example2.go
@@ -0,0 +1,31 @@
+package main
+
+// declare a struct called Movie
+// add the following fields:
+// - Title (string)
+// - Released (bool)
+// - Length (int)
+
+import "fmt"
+
+type Movie struct {
+	Title string
+	Released bool
+	Length int
+}
+
+func main() {
+	// declare a variable called "movie" of type "Movie"
+	var movie Movie
+
+	// Set the Title to "Wizard of Oz"
+	movie.Title = "Wizard of Oz"
+	// Set the Released variable to "true"
+	movie.Released = true
+	// Set Length to 125
+	movie.Length = 125
+
+	// Print the value of "movie" out
+	// hint: you can use fmt.Println(movie)
+	fmt.Println(movie)
+}
